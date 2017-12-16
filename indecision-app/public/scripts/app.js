17c61aff91f4bf3e40dee5651072bbfbf80af54e
David <>
2017-12-15 18:28:20 -0600

    10. Exploring JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,9 +5,32 @@ console.log('App.js is running!');
 //JSX
 
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
+    )
 );
 
 var appRoot = document.getElementById('app');
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,16 @@ console.log('App.js is running!');
 
 //JSX
 
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This some info</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
