9e2f7eb09f8da6e0c0870bc80dd81125a693559e
David <>
2018-02-07 12:50:34 -0600

    Setting up webpack and babel
    Setting up webpack and babel etc.
    
    Need to remove bundle.js from source control going forward

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,19 +1,46 @@
 // import './utils';
 // default can be called anything you want! This is the subtract function from utils
-import anythingIWant, {square, add} from "./utils.js";
-import isSenior, {isAdult, canDrink} from "./person.js";
+// import anythingIWant, {square, add} from "./utils.js";
+// import isSenior, {isAdult, canDrink} from "./person.js";
+//
+// console.log('app.js is running!!!!!');
+// console.log(square(4));
+// console.log(add(2, 3));
+// console.log(anythingIWant(100, 81));
+//
+//
+// console.log('An adult?', isAdult(13));
+// console.log('Can drink?', canDrink(13));
+// console.log('An adult?', isAdult(18));
+// console.log('Can drink?', canDrink(18));
+// console.log('An adult2?', isAdult(21));
+// console.log('Can drink2?', canDrink(21));
+// console.log('Is senior?', isSenior(64));
+// console.log('Is senior2?', isSenior(65));
 
-console.log('app.js is running!!!!!');
-console.log(square(4));
-console.log(add(2, 3));
-console.log(anythingIWant(100, 81));
+// installing and import from npm packages
+// yarn add validator@8.0.0
 
+// import validator from 'validator';
+//
+// console.log(validator.isEmail('test@gmail.com'));
 
-console.log('An adult?', isAdult(13));
-console.log('Can drink?', canDrink(13));
-console.log('An adult?', isAdult(18));
-console.log('Can drink?', canDrink(18));
-console.log('An adult2?', isAdult(21));
-console.log('Can drink2?', canDrink(21));
-console.log('Is senior?', isSenior(64));
-console.log('Is senior2?', isSenior(65));
+// do with react
+// yarn add react@16.0.0 react-dom@16.0.0
+// yarn add react@16.0.0 react-dom@16.0.0
+// note: need to work around by using createElement because we need Babel and support for JSX
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// const template = React.createElement('p', {}, 'Testing 123');
+// ReactDOM.render(template, document.getElementById('app'));
+
+// Teaching WebPack how to compile jsx
+// Loader lets you customize behavior of WebPack see webpack.config.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>THIS IS JSX FROM WEBPACK</p>;
+ReactDOM.render(template, document.getElementById('app'));
--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -6,5 +6,22 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    // Setting up loader for babel
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            // What files do we actually want to run this on?
+            // Does the file end in .js
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 };
+
+// loader
+// yarn add babel-core@6.25.0 babel-loader@7.1.1
+// similar to babel-cli except it allows you to run babel from tools such as webpack; babel-loader teach webpack how to load babel
+
+
+// After loader set up, must add .babelrc to root of project and specify presets
