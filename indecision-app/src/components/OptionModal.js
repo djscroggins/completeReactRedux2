9755bf423dc0ea9c54e0e0efc50c63b3ce79e437
David <>
2018-02-21 16:09:22 -0600

    Lecture 72 and before
    Styling the modal, mobile considerations

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -8,11 +8,13 @@ const OptionModal = (props) => (
         // Takes function when user closes modal, can close modal by clicking background, hitting escape etc
         onRequestClose={props.handleClearSelectedOption}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
+        <h3 className="modal__title">Selected Option</h3>
         {/*If value, then insert value in p tage*/}
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Okay</button>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
     </Modal>
     );
 
