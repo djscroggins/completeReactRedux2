7034238dec65fcc47cf80dc2a20598a2327f0ea4
David <>
2018-02-21 15:16:27 -0600

    Lecture 70
    Styling the widget

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -95,15 +95,21 @@ export default class IndecisionApp extends React.Component {
                         // Pass method to choose option to Action component
                         handlePick={this.handlePick}
                     />
-                    <Options
-                        options={this.state.options}
-                        // Pass method to delete option as prop  to Options component
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption}
-                    />
-                    <AddOption
-                        handleAddOption={this.handleAddOption}
-                    />
+
+                    <div className="widget">
+                        <Options
+                            options={this.state.options}
+                            // Pass method to delete option as prop  to Options component
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption
+                            handleAddOption={this.handleAddOption}
+                        />
+
+                    </div>
+
+
 
                 </div>
 
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -6,11 +6,18 @@ import Option from './Option';
 const Options = (props) => (
         <div>
             {/*Call props method from IndecisionApp component*/}
-            <button
-                className="button button--link"
-                onClick={props.handleDeleteOptions}>Remove all
-            </button>
-            {props.options.length === 0 ? <p>Please add an option to get started</p> : null}
+            <div className="widget-header">
+
+                <h3 className="widget-header__title">Your Options</h3>
+                <button
+                    className="button button--link"
+                    onClick={props.handleDeleteOptions}
+                >
+                    Remove all
+                </button>
+            </div>
+
+            {props.options.length === 0 ? <p className="widget__message">Please add an option to get started</p> : null}
             {/*key needed for iterators; in this case just assign option name as key*/}
             {props.options.map((o) => (
                     <Option
